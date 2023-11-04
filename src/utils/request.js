fecf9e1d8233e94f2f4e3c18e7809b8ef4e6b007
Hai HUANG <101720804+maple367@users.noreply.github.com>
2023-11-05 05:01:22 +0800

    更新 request.js
    修改bilibili反代理

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ export const getResp = (url, options = {}) => fetch(url, { referrer: '', referre
 export const get = (url, options) => getResp(url, options).then(r => r.json());
 
 export const corsGetResp = (url, options) =>
-  fetch('https://blc-proxy.lolicon.app', {
+  fetch('https://blc-proxy.maple367.eu.org', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ url, ...pick(options, ['method', 'headers', 'body']) }),
